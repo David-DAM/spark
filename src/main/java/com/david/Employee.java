ff64f39e87bbb3025564b7f8da7136fa406af5b5
David <david11jv@gmail.com>
2024-12-24 18:08:53 +0100

    Add exercises

---
+++ src/main/java/com/david/Employee.java
@@ -0,0 +1,53 @@
+package com.david;
+
+public class Employee {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String city;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
--- src/main/java/com/david/Main.java
+++ src/main/java/com/david/Main.java
@@ -1,22 +1,79 @@
 package com.david;
 
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.apache.spark.sql.functions.max;
+import static org.apache.spark.sql.functions.min;
+
 public class Main {
     public static void main(String[] args) {
         try (final SparkSession sparkSession = SparkSession.builder().appName("Spark Application").master("local").getOrCreate();
              final JavaSparkContext sparkContext = new JavaSparkContext(sparkSession.sparkContext())) {
 
-            List<Integer> list = Stream.iterate(0, i -> i + 1).limit(10).collect(Collectors.toUnmodifiableList());
+            String inputCsvFilePath = "src/main/resources/employees.csv";
+            String outputCsvFilePath = "src/main/resources/result.csv";
 
-            JavaRDD<Integer> parallelize = sparkContext.parallelize(list).filter(e -> e % 2 == 0);
-            System.out.println("Count of elements: " + parallelize.count());
+            Dataset<Row> df = sparkSession.read().format("csv")
+                    .option("header", "true")
+                    .option("inferSchema", "true")
+                    .load(inputCsvFilePath);
+
+            Dataset<Row> rowDataset = maxAndMin(df);
+            saveData(rowDataset, outputCsvFilePath);
+            sparkContext.stop();
         }
+    }
+
+    private static void saveData(Dataset<Row> rowDataset, String outputCsvFilePath) {
+        rowDataset.write()
+                .format("csv")
+                .option("header", "true")
+                .mode("overwrite")
+                .save(outputCsvFilePath);
+    }
+
+    private static Dataset<Row> maxAndMin(Dataset<Row> df) {
+        return df.agg(max("salary"), min("salary"));
+    }
+
+    private static void parallelizeCollection(JavaSparkContext sparkContext) {
+        List<Integer> list = Stream.iterate(0, i -> i + 1).limit(10).collect(Collectors.toUnmodifiableList());
+
+        JavaRDD<Integer> parallelize = sparkContext.parallelize(list).filter(e -> e % 2 == 0);
+        System.out.println("Count of elements: " + parallelize.count());
+    }
+
+    private static Dataset<Row> datasetGroupBy(Dataset<Row> df) {
+
+        return df.groupBy("city").max("salary");
+    }
+
+    private static void rddGroupBy(JavaSparkContext sparkContext, String csvFilePath) {
+        JavaRDD<Employee> employeeJavaRDD = sparkContext.textFile(csvFilePath).map(line -> {
+            String[] fields = line.split(",");
+            Employee employee = new Employee();
+            employee.setId(Long.valueOf(fields[0]));
+            employee.setName(fields[1]);
+            employee.setAge(Integer.parseInt(fields[2]));
+            employee.setCity(fields[3]);
+            employee.setSalary(Double.parseDouble(fields[4]));
+            return employee;
+        });
+
+        JavaPairRDD<String, Iterable<Employee>> employeeByCities = employeeJavaRDD.groupBy(Employee::getCity);
+
+        employeeByCities.foreach(tuple2 -> {
+            System.out.println("City: " + tuple2._1);
+            tuple2._2().forEach(System.out::println);
+        });
     }
 }
